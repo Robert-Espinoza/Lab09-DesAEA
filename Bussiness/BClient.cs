5efd313238541f9732ecdaaead70d1b50c72eb33
Robert-Espinoza <robert.espinoza.s@tecsup.edu.pe>
2023-11-08 23:58:39 -0500

    crud completo

---
+++ Bussiness/BClient.cs
@@ -0,0 +1,79 @@
+﻿using Data;
+using Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussiness
+{
+    public class BClient
+    {
+
+        public List<Client> GetByName(string Name)
+        {
+
+            List<Client> result = new List<Client>();
+
+            DClient dClient = new DClient();
+
+            var client = dClient.ListarClientes();
+
+            foreach (var item in client) 
+            {
+                
+                if (item.Name == Name)
+                {
+
+                    result.Add(item);
+
+                }
+
+            }
+
+            return result;
+
+        }
+
+        public List<Client> GetById(int id)
+        {
+
+            List<Client> result = new List<Client>();
+
+            DClient dClient = new DClient();
+
+            var client = dClient.ListarClientes();
+
+            foreach (var item in client)
+            {
+
+                if (item.Id == id)
+                {
+
+                    result.Add(item);
+
+                }
+
+            }
+
+            return result;
+
+        }
+
+        public List<Client> GetAll()
+        {
+            List<Client> result = new List<Client>();
+            DClient dClient = new DClient();
+            var client = dClient.ListarClientes();
+
+            foreach (var item in client)
+            {
+                result.Add(item);
+
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Web/Controllers/CustomController.cs
@@ -0,0 +1,101 @@
+﻿using Bussiness;
+using Entity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class CustomController : Controller
+    {
+        // GET: CustomController
+        public ActionResult Index()
+        {
+            
+            List<Client>customers = new List<Client>();
+
+            BClient bCustomer = new BClient();
+
+            List<CustomerModel> models = new List<CustomerModel>();
+
+            customers = bCustomer.GetAll();
+
+            models = customers.Select(x=> new CustomerModel
+            {   Id = x.Id,
+                Name = x.Name,
+                Phone = x.Phone
+
+            }).ToList();
+            return View(models);
+        }
+
+        // GET: CustomController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CustomController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CustomController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CustomController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CustomController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
